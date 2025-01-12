8230b1aeb8381b4ea0de1d5dff69b153297b4c70
thoaidtse <thanhthoai90813@gmail.com>
2025-01-12 17:42:12 +0700

    main
    update

---
+++ Assets/Scripts/AnimationStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    internal class AnimationStrings
+    {
+        internal static string isMoving = "isMoving";
+        internal static string isRunning = "isRunning";
+    }
+}
