32fad6b680228af5a6d8861755344c42182901e7
KietBap <kietanh2622003@gmail.com>
2025-01-18 16:49:59 +0700

    update code (2:15:00)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,35 +1,106 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 10f;
-    Vector2 moveInput;
-    public bool IsMoving { get; private set; }
+    public float runSpeed = 13f;
+    public float airWalkSpeed = 3f;
+	private float jumpImpulse = 10f;
+	Vector2 moveInput;
+    TouchingDirections touchingDirections;
 
-    Rigidbody2D rb;
+    public float CurrentMoveSpeed { get
+        {
+            if (IsMoving && !touchingDirections.IsOnWall)
+            {
+                if (touchingDirections.IsGrounded)
+                {
+					if (IsRunning)
+					{
+						return runSpeed;
+					}
+					else
+					{
+						return walkSpeed;
+					}
+				}
+                else
+                {
+                    return airWalkSpeed;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
 
-    private void Awake()
+
+    [SerializeField] 
+    private bool _isMoving = false;
+
+    public bool IsMoving 
+    {   
+        get 
+        { 
+            return _isMoving;
+        } private set 
+        { 
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        } 
+    }
+
+	[SerializeField]
+	private bool _isRunning = false;
+
+    public bool IsRunning 
+    { 
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool(AnimationStrings.isRunning, value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+	public bool IsFacingRight { get { return _isFacingRight; } private set 
+        {
+            if(_isFacingRight != value) 
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+
+            _isFacingRight = value;
+        } }
+
+	Rigidbody2D rb;
+    Animator animator;
+	
+
+	private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     [System.Obsolete]
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput .x * walkSpeed * Time.fixedDeltaTime, rb.velocity.y);  
+        rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.linearVelocity.y);
+
+        animator.SetFloat(AnimationStrings.yVelocity, rb.linearVelocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -37,5 +108,39 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+	private void SetFacingDirection(Vector2 moveInput)
+	{
+		if(moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if(moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight= false;
+        }
+	}
+
+	public void OnRun(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            IsRunning = true;
+        }else if(context.canceled)
+        {
+            IsRunning=false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger(AnimationStrings.jump);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpImpulse);
+        }
     }
 }
